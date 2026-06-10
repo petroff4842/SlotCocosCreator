d8ebc997ccad97eae42e6a2e835c79e7d4a97f2c
petroff4842 <petroff4842@gmail.com>
2026-06-10 14:46:54 +0300

    setup mask for reel area, extract reel setup logic

--- assets/scripts/controller/SlotGame.ts
+++ assets/scripts/controller/SlotGame.ts
@@ -1,7 +1,7 @@
-import { _decorator, Component, Node } from "cc";
-import { AssetLoader } from "../services/AsssetsLoader";
+import { _decorator, Component, Node, UITransform } from "cc";
+import { AssetLoader, SymbolFrames } from "../services/AsssetsLoader";
 import { ReelView } from "../view/ReelView";
-import { SlotConfig, SymbolId } from "../config/SlotConfig";
+import { ALL_SYMBOLS, SlotConfig, SymbolId } from "../config/SlotConfig";
 const { ccclass, property } = _decorator;
 
 @ccclass("SlotGame")
@@ -13,8 +13,36 @@ export class SlotGame extends Component {
     const frames = await AssetLoader.loadSymbolFrames();
     console.log("Loaded symbol frames:", frames.size);
 
-    const totalWidth = SlotConfig.REEL_COUNT * SlotConfig.SYMBOL_WIDTH;
+    this.setupReelArea();
+    this.createReels(frames);
+  }
 
+  private setupReelArea(): void {
+    const reelArea = this.reelsRoot.parent?.parent;
+    const reelMask = this.reelsRoot.parent;
+
+    reelArea
+      ?.getComponent(UITransform)
+      ?.setContentSize(SlotConfig.REEL_AREA_WIDTH, SlotConfig.REEL_AREA_HEIGHT);
+    reelMask
+      ?.getComponent(UITransform)
+      ?.setContentSize(SlotConfig.REEL_AREA_WIDTH, SlotConfig.REEL_AREA_HEIGHT);
+  }
+
+  private createRandomSymbols(): SymbolId[] {
+    const symbols: SymbolId[] = [];
+    for (let i = 0; i < SlotConfig.VISIBLE_ROWS; i++) {
+      const randomSymbol =
+        ALL_SYMBOLS[Math.floor(Math.random() * ALL_SYMBOLS.length)];
+
+      symbols.push(randomSymbol);
+    }
+
+    return symbols;
+  }
+
+  private createReels(frames: SymbolFrames): void {
+    const totalWidth = SlotConfig.REEL_COUNT * SlotConfig.SYMBOL_WIDTH;
     const startX = -totalWidth / 2 + SlotConfig.SYMBOL_WIDTH / 2;
 
     for (let reelIndex = 0; reelIndex < SlotConfig.REEL_COUNT; reelIndex++) {
@@ -24,7 +52,7 @@ export class SlotGame extends Component {
       reelNode.setPosition(startX + reelIndex * SlotConfig.SYMBOL_WIDTH, 0, 0);
 
       const reelView = reelNode.addComponent(ReelView);
-      reelView.init(frames, [SymbolId.Wild, SymbolId.L1, SymbolId.H3]);
+      reelView.init(frames, this.createRandomSymbols());
     }
   }
 }
